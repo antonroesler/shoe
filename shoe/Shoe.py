6959182f35a7bdfef9a80c3547e845d0ca174df2
antoroesler <anton.roesler98@gmx.de>
2021-02-28 17:05:03 +0100

    introduce Shoe class

--- shoe/Shoe.py
+++ shoe/Shoe.py
@@ -1,8 +1,21 @@
 
 class Shoe:
-    def __init__(self, name, mileage=0):
+    def __init__(self, name):
         self.name = name
-        self.mileage = mileage
+        self.mileage = 0
+        self.runs = 0
+        self.max_mileage = False
 
     def print_info(self):
-        print(f"Shoe {self.name} has {self.mileage} kilometers.")
+        print(f"INFO: -- {self.name} --")
+        print(f"- {self.mileage} total kilometers")
+        print(f"- {self.runs} total runs")
+        if self.max_mileage:
+            print(f"- {self.max_mileage - self.mileage} kilometers left before replacement")
+
+    def add_mileage(self, distance):
+        self.mileage += distance
+        self.runs += 1
+
+    def set_max_mileage(self, mileage):
+        self.max_mileage = float(mileage)
