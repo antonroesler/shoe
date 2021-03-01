1912a33f37b5b30914040727419695178e80029a
antoroesler <anton.roesler98@gmx.de>
2021-03-01 15:17:28 +0100

    added short man page

---
+++ shoe/manual.py
@@ -0,0 +1,11 @@
+
+def man_page():
+    print()
+    print("- - - - MAN - - - -")
+    print("shoe create <name>           to add a new shoe")
+    print("shoe add <name> <mileage>    to add mileage to a shoe")
+    print("shoe [info] <name>           to retrieve a shoe's infos")
+    print("shoe list                    to list all shoes")
+    print("shoe rm <name>               to delete a shoe")
+    print("- - - - END - - - -")
+    print()
